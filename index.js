7b3d897624fbbb47cd31ed9856c4b2ba63db53fa
HyanCat <hyancat@live.cn>
2015-05-29 15:26:50 +0800

    log with gulp util.

--- index.js
+++ index.js
@@ -2,35 +2,36 @@ var gutil = require('gulp-util');
 
 var ElixirGroup = {
 	// task container, KV: taskName => taskFuncion()
-	tasks: {},
+	tasks   : {},
 	// register task group.
-	register: function(taskName, taskFuncion) {
+	register: function (taskName, taskFuncion) {
 		this.tasks[taskName] = taskFuncion;
-		console.log('register: ' + taskName);
+		gutil.log(gutil.colors.white('Register Group:', taskName));
 	},
 	// run task group.
-	run: function(taskName) {
-		console.log('begin run: ' + taskName);
+	run     : function (taskName) {
+		gutil.log(gutil.colors.white('Starting Group:', taskName));
 		var taskFuncion = this.tasks[taskName];
 		if (typeof(taskFuncion) == 'function') {
 			taskFuncion();
 		}
-		console.log('finish run: ' + taskName);
+		gutil.log(gutil.colors.white('Finished Group:', taskName));
 		this.finally();
 	},
 	// run all task group.
-	runAll: function() {
+	runAll  : function () {
 		for (var taskName in this.tasks) {
 			this.run(taskName);
 		}
 		this.finally();
 	},
 	// deal something should be shared.
-	finally: function() {}
+	finally : function () {
+	}
 };
 
 
-ElixirGroup.start = function() {
+ElixirGroup.start = function () {
 	// if the args contain the taskName with -- prefix, then run the task.
 	var hasGroup = false;
 	for (var arg in gutil.env) {
